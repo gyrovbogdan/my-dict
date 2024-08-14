a1918fa4c7943edc75f0bd272c91c565b37731b0
gyrovbogdan <gyrovbogdan@gmail.com>
2024-08-14 12:28:44 +0300

    Fix bug

--- resources/js/utils/events.js
+++ resources/js/utils/events.js
@@ -44,6 +44,7 @@ export function updateEventListeners(dictionary) {
         $this.on("blur", function () {
             if (dataChanged) return;
             $this.closest("tr").html(buffer);
+            dictionary.addEventListeners();
         });
     });
 }
